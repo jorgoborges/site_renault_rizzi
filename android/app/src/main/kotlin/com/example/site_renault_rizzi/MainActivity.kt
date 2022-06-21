b08cfec1bbbc456f3445a882008fe86ad06124c2
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-06-21 09:27:52 -0300

    Validação 20/06/22 02:03

--- android/app/src/main/kotlin/com/example/site_renault_rizzi/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/site_renault_rizzi/MainActivity.kt
@@ -2,25 +2,5 @@ package com.example.site_renault_rizzi
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity() : FlutterActivity(), android.os.Parcelable {
-    constructor(parcel: android.os.Parcel) : this() {
-    }
-
-    override fun writeToParcel(parcel: android.os.Parcel, flags: Int) {
-
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : android.os.Parcelable.Creator<MainActivity> {
-        override fun createFromParcel(parcel: android.os.Parcel): MainActivity {
-            return MainActivity(parcel)
-        }
-
-        override fun newArray(size: Int): Array<MainActivity?> {
-            return arrayOfNulls(size)
-        }
-    }
+class MainActivity: FlutterActivity() {
 }
