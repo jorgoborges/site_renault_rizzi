4f8e090fd6902754abb262d47c76f52062208511
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-06-21 10:15:55 -0300

    21062022

--- lib/model_vwco/second_level_item_2_page.dart
+++ lib/model_vwco/second_level_item_2_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:site_renault_rizzi/interface/admin_scaffold.dart.dart';
-import 'package:flutter_widget_from_html_core/flutter_widget_from_html_core.dart';
-import 'package:site_renault_rizzi/interface/chart_bi_one.dart';
+
 
 class SecondLevelItem2Page extends StatelessWidget {
   @override
@@ -11,9 +10,9 @@ class SecondLevelItem2Page extends StatelessWidget {
       body: Container(
         alignment: Alignment.topLeft,
         padding: const EdgeInsets.all(10),
-        child: HtmlWidget(
-          '<iframe title="Acompanhamento de reuniões Mardisa" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1816f326-ad24-4c74-998f-11721c3ad328&autoAuth=true&ctid=bae03bf5-f801-4f8b-adba-c177d66c8180&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLWJyYXppbC1zb3V0aC1iLXByaW1hcnktcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D" frameborder="0" allowFullScreen="true"></iframe>',
-          factoryBuilder: () => MyWidgetFactory(),
+        child: Text(
+          'SecondLevelItem2',
+
         ),
       ),
     );
