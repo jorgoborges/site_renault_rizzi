b0f02170845b8533f3bee056df06ed66e51d6067
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-06-22 15:04:34 -0300

    22/06/22 13:25 - Jorge

--- lib/model_vwco/second_level_item_1_page.dart
+++ lib/model_vwco/second_level_item_1_page.dart
@@ -14,7 +14,7 @@ class SecondLevelItem1Page extends StatelessWidget {
         alignment: Alignment.center,
         padding: EdgeInsets.only(left: 20, top:10),
         child: Image.network(
-          'https://snz04pap002files.storage.live.com/y4mspK2xLddOnEhkdNgMHNX6dryArVxMGXbBvZY1k1E94w6VmZMMb2QZXbfkBM00fAAf_3Atb1QHkXnoxsZFZSDgYO7hnFK8UGU_H1EU0o6SGuU6KrdHIHFIwJt7WY68Bn1S2EPcY96ryDnQHi7uRjhVRn0ef8asTq9ZT_AAPaBDZYIuf8IlrxpqOvfJ4__GNAOe1ws8RvHlaEr911bWuqe1_PD0Ma-ZpoDlCU0RYttSUM?encodeFailures=1&width=1633&height=842',
+          'https://snz04pap002files.storage.live.com/y4mUIMOU51w8Prh6kW8gzSlFcKR7WOXZbNVJhR9GIxmtVpagbPF1OVB9-nwV-lYzhOcJ-J083dqBDx--7pDRZ_n_enRmsGGJLmCABinl-7dn0cn4cxYKZzZ3Wmlm72230IZSmDy9pNyTvUzrRU64idrj3BbSB_d9QmGsCP_CFqZvH2z7oIboo0TPcG_lE_s3zsH8dAlnCRxExfPFfGswT0G4i51ig8qvPEtk9sjC529MTQ?encodeFailures=1&width=1691&height=821',
           fit: BoxFit.fill,
         ),
       ),
