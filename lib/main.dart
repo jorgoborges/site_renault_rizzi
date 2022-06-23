00501d5405f12ad061998f80dc5426b1fd10eece
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-06-23 17:30:55 -0300

    23/06/22 17:30 - Jorge

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:site_renault_rizzi/login/login.dart';
+
 import 'package:site_renault_rizzi/login/login_page.dart';
 
 void main() async {
@@ -14,8 +14,6 @@ class Initial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: LoginPage());
+        debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
