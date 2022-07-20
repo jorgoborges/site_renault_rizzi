7e1929dabd1062462bd0288315d64a320d931842
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-07-20 20:43:49 -0300

    Versão 20/07/2022 Revisão 03 *****

--- lib/login/login_home.dart
+++ lib/login/login_home.dart
@@ -63,7 +63,7 @@ class LoginHome extends StatelessWidget {
             child: FittedBox(
               alignment: Alignment.center,
               child: Image(
-                image: AssetImage("assets/images/control.PNG"),
+                image: AssetImage("assets/images/RizziControlTower.png"),
                 fit: BoxFit.contain,
               ),
             ),
