dfdef905350bfe1084211d489bf7176653458f46
jorgo <consultor01remoto@rizziconsulting.com.br>
2022-07-25 15:37:54 -0300

    Versão 25/07/2022 Revisão 11 *****

--- lib/login/login_home.dart
+++ lib/login/login_home.dart
@@ -30,89 +30,128 @@ class LoginHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          TextButton(
-            onPressed: () {},
-            style: TextButton.styleFrom(
-              padding: const EdgeInsets.all(15),
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                  child: Image(
-                    image: AssetImage("assets/images/image1.png.png"),
-                    fit: BoxFit.fill,
-                    height: 100,
-                    width: 100,
+        appBar: AppBar(
+          actions: [
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(15),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    child: Image(
+                      image: AssetImage("assets/images/image1.png.png"),
+                      fit: BoxFit.fill,
+                      height: 100,
+                      width: 100,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ),
+          ],
+          backgroundColor: const Color(0xff041e42),
+          title: const Text('RMS - RIZZI MANAGEMENT SYSTEM'),
+        ),
+        body: Stack(children: [
+          Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(
+              top: 3,
+            ),
+            child: Image(
+              image: AssetImage("assets/images/image2.png.png"),
+              fit: BoxFit.fill,
+              height: 400,
+              width: 1000,
             ),
           ),
-        ],
-        backgroundColor: const Color(0xff041e42),
-        title: const Text('RMS - RIZZI MANAGEMENT SYSTEM'),
-      ),
-      body: Stack(children: [
-        Container(
-          alignment: Alignment.topCenter,
-          padding: EdgeInsets.only(
-            top: 3,
-          ),
-          child: Image(
-            image: AssetImage("assets/images/image2.png.png"),
-            fit: BoxFit.fill,
-            width: 900,
-            height: 400,
-          ),
-        ),
-        Flexible(
-          child: Container(
-            alignment: Alignment.center,
-            child: Column(
-              children: <Widget>[
-                Row(
+          Padding(
+            padding: const EdgeInsets.only(left: 150, right: 130),
+            child: Container(
+              alignment: Alignment.center,
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    Flexible(
-                        child: Container(
-                      alignment: Alignment.center,
-                      padding: EdgeInsets.only(
-                        left: 80,
-                        top: 430,
-                      ),
-                      child: TextFormField(
-                        controller: _controladorEmail,
-                        decoration: InputDecoration(
-                            filled: true,
-                            fillColor: Colors.white24,
-                            hintText: 'Email',
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(90.0)),
-                                borderSide:
-                                    BorderSide(color: Colors.red, width: 0.5)),
-                            prefixIcon: const Icon(
-                              Icons.email,
-                              color: Colors.black,
-                            )),
-                        keyboardType: TextInputType.emailAddress,
-                      ),
-                    )),
                     Padding(
-                        padding: EdgeInsets.only(
-                      right: 20,
-                    )),
-
+                      padding: EdgeInsets.only(top: 420),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            style: TextStyle(color: Colors.black87),
+                            controller: _controladorEmail,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontFamily: "WorkSansLight",
+                                  fontSize: 17.0),
+                              filled: true,
+                              fillColor: Colors.white24,
+                              hintText: "E-mail",
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(90.0)),
+                                  borderSide: BorderSide(
+                                      color: Colors.white24, width: 0.5)),
+                              prefixIcon: const Icon(
+                                Icons.email,
+                                color: Colors.black,
+                              ),
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                    ),
+                    TextFormField(
+                      style: TextStyle(color: Colors.black87),
+                      controller: _controladorSenha,
+                      decoration: InputDecoration(
+                        hintStyle: TextStyle(
+                            color: Colors.black,
+                            fontFamily: "WorkSansLight",
+                            fontSize: 17.0),
+                        filled: true,
+                        fillColor: Colors.white24,
+                        hintText: "Senha",
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(90.0)),
+                            borderSide:
+                                BorderSide(color: Colors.white24, width: 0.5)),
+                        prefixIcon: const Icon(
+                          Icons.lock_outline,
+                          color: Colors.black,
+                        ),
+                      ),
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: true,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 5, top:80),
+                    ),
+                    SizedBox(
+                      height: 80,
+                      width: 90,
+                      child: new FloatingActionButton(
+                          backgroundColor: Colors.black,
+                          child: Text(
+                            "Entrar",
+                            style:
+                                TextStyle(fontSize: 18.0, color: Colors.white),
+                          ),
+                          onPressed: () => {newlogin(context)}),
+                    ),
                   ],
                 ),
-
-              ],
+              ),
             ),
           ),
-        ),
-      ]),
-    );
+        ]));
   }
 }
